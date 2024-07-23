a90e539c418b1283a7bc84850a069218e0097711
9oHigh <wkdska957@naver.com>
2024-07-23 17:03:00 +0900

    Initial Commit

---
+++ usket.SSlash/usket_SSlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  usket_SSlashApp.swift
+//  usket.SSlash
+//
+//  Created by 이경후 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct usket_SSlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

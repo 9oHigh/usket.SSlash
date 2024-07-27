9b4cd705dbe732c1d496a3418ab2e96b4cbf6f05
9oHigh <wkdska957@naver.com>
2024-07-27 23:12:57 +0900

    fix: remove submodule and upload new project

---
+++ SSlash/SSlash/App/Source/SSlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSlashApp.swift
+//  SSlash
+//
+//  Created by 이경후 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SSlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

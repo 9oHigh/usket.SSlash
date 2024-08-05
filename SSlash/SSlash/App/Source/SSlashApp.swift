06950c11b2c23ecbf144651656ef55057da35ef3
9oHigh <wkdska957@naver.com>
2024-08-05 14:09:03 +0900

    feat: add firebase for admob

--- SSlash/SSlash/App/Source/SSlashApp.swift
+++ SSlash/SSlash/App/Source/SSlashApp.swift
@@ -6,12 +6,40 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct SSlashApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        setNavigationBarAppearance()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+extension SSlashApp {
+    
+    private func setNavigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(AppColor.nomalBackground.color)
+        appearance.shadowColor = .clear
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
